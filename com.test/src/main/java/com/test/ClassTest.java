79a9aed09a34b8b1f6c7322daf5c89a8026b58a9
dell <dell@Laxmipriya>
2019-04-19 23:58:58 +0530

    code changes done

--- com.test/src/main/java/com/test/ClassTest.java
+++ com.test/src/main/java/com/test/ClassTest.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,6 +11,8 @@ public void StartBrowser(){
 System.setProperty("webdriver.chrome.driver", "C:\\Users\\dell\\Desktop\\chromedriver.exe");
 WebDriver driver=new ChromeDriver();
 driver.get("https://www.google.com/");
+driver.findElement(By.xpath(""));
+driver.findElement(By.id(""));
 
 }
 
