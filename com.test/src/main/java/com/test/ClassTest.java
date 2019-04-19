593022e2eb04677665b3471d7ccf328a8889774e
dell <dell@Laxmipriya>
2019-04-19 23:52:51 +0530

    code pushed

---
+++ com.test/src/main/java/com/test/ClassTest.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ClassTest {
+@Test
+public void StartBrowser(){
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\dell\\Desktop\\chromedriver.exe");
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.google.com/");
+
+}
+
+}
